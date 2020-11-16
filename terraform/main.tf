a4d17530ab6a1ad60fa6bbb5978055dc59529fc4
Roman Melnikov <roman.melnikov@serokell.io>
2020-11-16 17:18:42 +0300

    [OPS-1099] Add DNS record for new machine

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,9 +36,8 @@ resource "hcloud_ssh_key" "rvem" {
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDEmUvzbwZM3179iOQPGiXF/Xwhyh5wxfvzbGX9HP5WIQIKkMwoLstt0OaHR+U8YoxhrHK0YiZ99CH3waACoLn+xd/8xBitL0zQaHuvrol8Y2G4H5ig8T3D/w3m88OzoQkwdibDL8Rtxuu0XlEpIXi526LAiK5p/UWL+xHacVNXQlEyPZDzWfLQpmyB36GcADYK4pXMqri/0x+7WAauUlK8Qb0fYF1dVyGNNqqLV3bop4dtNvchQ8kQFqLhT/E3yHek5fLx9Mo3OyJsGoma4x4vF+B5AC0dqpUbmljIJhX+rXdtwo/9g+SlGrJ5LV57ktPSASpTeo7BSv4rf0Uv8kT7zJXMQEPcNzN6Xh7wNVMN8lLsoyQwHJQcK/GmTw6I+ywJnnO+1Ku+mEB1qm6hX3JU7+u6a7CB7IuFUE23qM41RSwMhCma84wIZd3lu0O7j3YiUTinSdyT4QJxIEOtZ1iNNyoHOibrJaRxjVQGmQvoOe/A/+odecu7aHKo4JqjTaG6yTWmkDZyCwIsRCh2jTRhJ+Vy6cYJediMk2tYjKZWGQKP79tqiOkiHUDglU3n6wwrn1EDDV7aGg2gW3vLV46/LSm9oEVmvLAfuiPWyJCI/TJ9VZ1037HdIklPsmLGnx0yDwTUUjwtD9lA9fBfyXE1RENO0B/aQByRgn/GL+PJRQ== rvembox@gmail.com"
 }
 
-# TODO: add dns record for new server
 resource "hcloud_server" "bunda" {
-  name = "node1"
+  name = "bunda"
   image = "ubuntu-20.04"
   server_type = "cx31"
   ssh_keys = [ hcloud_ssh_key.rvem.id ]
@@ -49,6 +48,22 @@ resource "hcloud_server" "bunda" {
     runcmd:
       - curl https://raw.githubusercontent.com/elitak/nixos-infect/master/nixos-infect | NIX_CHANNEL=nixos-20.03 bash 2>&1 | tee /tmp/infect.log
 EOF
+}
+
+resource "aws_route53_record" "bunda_aquarius_serokell_team_ipv4" {
+  zone_id = aws_route53_zone.aquarius_serokell_team.zone_id
+  name    = "bunda.aquarius.serokell.team"
+  type    = "A"
+  ttl     = "60"
+  records = [hcloud_server.bunda.ipv4_address]
+}
+
+resource "aws_route53_record" "bunda_aquarius_serokell_team_ipv6" {
+  zone_id = aws_route53_zone.aquarius_serokell_team.zone_id
+  name    = "bunda.aquarius.serokell.team"
+  type    = "AAAA"
+  ttl     = "60"
+  records = [hcloud_server.bunda.ipv6_address]
 }
 
 output "aquarius_ns" {value = [ aws_route53_zone.aquarius_serokell_team.name_servers ]}
