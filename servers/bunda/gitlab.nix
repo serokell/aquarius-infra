97034fc483d40b19e8e16b277ae3320ffdcff031
Roman Melnikov <roman.melnikov@serokell.io>
2020-11-17 17:24:09 +0300

    Generalize gitlab runner creation

--- servers/bunda/gitlab.nix
+++ servers/bunda/gitlab.nix
@@ -1,37 +1,18 @@
-{ config, pkgs, lib, ... }:
-let vs = config.vault-secrets.secrets;
-in {
+{ config, pkgs, lib, ... }@args:
+let
+  vs = config.vault-secrets.secrets;
+  gitlab = import ../../common/gitlab.nix args;
+in rec {
   vault-secrets.secrets.gitlab-runner = { };
-  services.gitlab-runner = let
-    shellRunner = registrationTokenFile: {
-      executor = "shell";
-      tagList = [ "nix-with-docker" ];
-      runUntagged = false;
-
-      registrationConfigFile = pkgs.writeText "gitlab-registration.sh" ''
-        CI_SERVER_URL="https://gitlab.com/"
-        REGISTRATION_TOKEN=$(cat ${lib.escapeShellArg registrationTokenFile})
-      '';
-
-      # gitlab-runner may reuse an existing checkout, but it will fail to clean
-      # it up if it contains non-writable directories (e.g. copied from the nix
-      # store by the previous job). This script makes all directories writable.
-      # Upstream issue: https://gitlab.com/gitlab-org/gitlab-runner/-/issues/164
-      preCloneScript = pkgs.writeShellScript "make-directories-writable.sh" ''
-        if [ -d "$CI_PROJECT_DIR" ]; then
-        # ignoring .git, find directories with 'w' bit unset and set the bit
-        find "$CI_PROJECT_DIR" \
-        -path "$CI_PROJECT_DIR/.git" -prune \
-        -o "(" -type d -a -not -perm -u=w ")" -exec chmod --verbose u+w {} ";"
-        fi
-      '';
-    };
-  in {
+  services.gitlab-runner = {
     enable = true;
     concurrent = 2;
     services = {
       # https://gitlab.com/morley-framework
-      morley-shell = shellRunner "${vs.gitlab-runner}/REG_TOKEN_MORLEY_SHELL";
+      morley-shell = gitlab.shellRunner "${vs.gitlab-runner}/REG_TOKEN_MORLEY_SHELL"
+        { runUntagged = false;
+          tagList = [ "nix-with-docker" ];
+        };
     };
   };
 
