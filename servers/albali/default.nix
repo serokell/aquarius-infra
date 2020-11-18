38c69f3a9a02c67aebc66406725ac104c94c9f86
Alexander Bantyev <balsoft@balsoft.ru>
2020-11-18 19:35:19 +0300

    Albali: add a mac builder

--- servers/albali/default.nix
+++ servers/albali/default.nix
@@ -3,6 +3,8 @@
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
 
+    ./builders.nix
+
     inputs.upload-daemon.nixosModules.upload-daemon
     ./upload-daemon.nix
 
