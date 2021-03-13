053cf9795d79a637221a8bafcb3a89b551512e09
Alexander Bantyev <balsoft@balsoft.ru>
2021-03-14 00:57:24 +0300

    Use vault-secrets repo and scripts

--- common.nix
+++ common.nix
@@ -1,16 +1,17 @@
-{ config, inputs, ... }: {
+{ config, inputs, ... }:
+let constellation = "aquarius";
+in {
   imports = [
     inputs.serokell-nix.nixosModules.common
     inputs.serokell-nix.nixosModules.serokell-users
-    inputs.serokell-nix.nixosModules.vault-secrets
+    inputs.vault-secrets.nixosModules.vault-secrets
   ];
 
-  networking.domain = "aquarius.serokell.team";
+  networking.domain = "${constellation}.serokell.team";
 
   vault-secrets = {
-    vaultPathPrefix = "kv/sys/aquarius";
+    vaultPrefix = "kv/sys/${constellation}/${config.networking.hostName}";
     vaultAddress = "https://vault.serokell.org:8200";
-    namespace = config.networking.hostName;
-    approlePrefix = "aquarius-${config.networking.hostName}";
+    approlePrefix = "${constellation}-${config.networking.hostName}";
   };
 }
