c98f826115c738311fb5da20afaf6a9b75fd4a06
Alexander Bantyev <balsoft@balsoft.ru>
2021-06-09 13:25:33 +0300

    Update hcloud version in terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -13,6 +13,6 @@ data "vault_generic_secret" "hcloud_token" {
 }
 
 provider "hcloud" {
-  version = "~> 1.22.0"
+  version = "~> 1.26.0"
   token = data.vault_generic_secret.hcloud_token.data["token"]
 }
